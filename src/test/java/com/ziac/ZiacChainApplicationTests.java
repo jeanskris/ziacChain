0925552ac58489810e742873d1e25aee94f8b99c
wwq <823509241@qq.com>
2017-10-16 20:13:13 +0800

    add test

--- src/test/java/com/ziac/ZiacChainApplicationTests.java
+++ src/test/java/com/ziac/ZiacChainApplicationTests.java
@@ -15,164 +15,251 @@ import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import static java.lang.Thread.sleep;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZiacChainApplicationTests {
-	private Logger logger = LoggerFactory.getLogger(ZiacChainApplicationTests.class);
-	@Test
-	public void contextLoads() {
-	}
-	@Test
-	public void gossipTest() {
-	try {
-			GossipSettings settings = new GossipSettings();
-			List<GossipMember> startupMembers = new ArrayList<>();
-			List<GossipService> clients = new ArrayList<>();
-			String myIpAddress = InetAddress.getLocalHost().getHostAddress();
-			String cluster = "My Gossip Cluster";
-			for (int i = 0; i < 4; ++i) {
-				URI u;
-				try {
-					u = new URI("udp://" + myIpAddress + ":" + (2000 + i));
-					System.out.println(u.toString());
-				} catch (URISyntaxException e) {
-					throw new RuntimeException(e);
-				}
+    private Logger logger = LoggerFactory.getLogger(ZiacChainApplicationTests.class);
 
-				startupMembers.add(new RemoteGossipMember(cluster, u, "", 0L, new HashMap()));
-			}
+    @Test
+    public void contextLoads() {
+    }
 
-			Iterator iterator = startupMembers.iterator();
+    @Test
+    public void gossipTest() {
+        try {
+            GossipSettings settings = new GossipSettings();
+            List<GossipMember> startupMembers = new ArrayList<>();
+            List<GossipService> clients = new ArrayList<>();
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster1";
+            for (int i = 0; i < 4; ++i) {
+                URI u;
+                try {
+                    u = new URI("udp://" + myIpAddress + ":" + (2000 + i));
+                    System.out.println(u.toString());
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
 
-			while (iterator.hasNext()) {
-				GossipMember member = (GossipMember) iterator.next();
-				GossipService gossipService = new GossipService(cluster, member.getUri(), "", new HashMap(), startupMembers, settings, (GossipListener) null, new MetricRegistry());
-				clients.add(gossipService);
-				gossipService.start();
-				sleep((long) (settings.getCleanupInterval() + 1000));
-			}
+                startupMembers.add(new RemoteGossipMember(cluster, u, "", 0L, new HashMap()));
+            }
 
-			((GossipService) clients.get(0)).shutdown();
+            Iterator iterator = startupMembers.iterator();
 
-		}  catch (UnknownHostException var10) {
-		var10.printStackTrace();
-	} catch (InterruptedException var11) {
-		var11.printStackTrace();
-	}
-	}
-	@Test
-	public  void  test2(){
+            while (iterator.hasNext()) {
+                GossipMember member = (GossipMember) iterator.next();
+                GossipService gossipService = new GossipService(cluster, member.getUri(), "", new HashMap(), startupMembers, settings, (GossipListener) null, new MetricRegistry());
+                clients.add(gossipService);
+                gossipService.start();
+                sleep((long) (settings.getCleanupInterval() + 1000));
+            }
 
-		try {
-			GossipSettings settings = new GossipSettings();
-			settings.setGossipInterval(1);
-			settings.setCleanupInterval(1000);
-			List<GossipService> clients = new ArrayList();
-			String myIpAddress = InetAddress.getLocalHost().getHostAddress();
-			String cluster = "My Gossip Cluster 1";
-			List<GossipMember> startupMembers = new ArrayList();
+            ((GossipService) clients.get(0)).shutdown();
 
-			URI seedUrl;
-			try {
-				seedUrl = new URI("udp://" + myIpAddress + ":" + 11111);
-			} catch (URISyntaxException var9) {
-				throw new RuntimeException(var9);
-			}
-			startupMembers.add(new RemoteGossipMember(cluster, seedUrl, "seed", 0L, new HashMap()));
-			GossipService gossipService0 = new GossipService(cluster, seedUrl, "", new HashMap(), startupMembers, settings, (GossipListener) null, new MetricRegistry());
-			gossipService0.start();
-			URI url;
-			try {
-				url = new URI("udp://" + myIpAddress + ":" + 50003);
-			} catch (URISyntaxException var9) {
-				throw new RuntimeException(var9);
-			}
-			GossipService gossipService = new GossipService(cluster,
-					url, "seed3", new HashMap(), startupMembers, settings,
-					new GossipListener() {
-						@Override
-						public void gossipEvent(GossipMember member, GossipState state) {
-							System.out.println(System.currentTimeMillis() + " "
-									+ member + " " + state);
-						}
-					},
-					new MetricRegistry());
-			clients.add(gossipService);
-			gossipService.start();
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
+    }
 
-			while(true) {
-				sleep(2000L);
-				if (!clients.isEmpty()) {
-					if (!clients.isEmpty()) {
-						List<LocalGossipMember> localGossipMemberList1 = clients.get(0).getGossipManager().getLiveMembers();
-						for (LocalGossipMember localGossipMember : localGossipMemberList1) {
-							System.out.print(" " + localGossipMember.getUri());
+    @Test
+    public void test2() {
 
-						}
-					}
-				}
-			}
-			//((GossipService) clients.get(0)).getGossipManager().shutdown();
-		} catch (UnknownHostException var10) {
-			var10.printStackTrace();
-		} catch (InterruptedException var11) {
-			var11.printStackTrace();
-		}
+        try {
+            GossipSettings settings = new GossipSettings();
+            settings.setGossipInterval(1);
+            settings.setCleanupInterval(1000);
+            List<GossipService> clients = new ArrayList();
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster 1";
+            List<GossipMember> startupMembers = new ArrayList();
 
-	}
+            URI seedUrl;
+            try {
+                seedUrl = new URI("udp://" + myIpAddress + ":" + 11111);
+            } catch (URISyntaxException var9) {
+                throw new RuntimeException(var9);
+            }
+            startupMembers.add(new RemoteGossipMember(cluster, seedUrl, "seed0", 0L, new HashMap()));
+            GossipService gossipService0 = new GossipService(cluster, seedUrl, "", new HashMap(), startupMembers, settings, (GossipListener) null, new MetricRegistry());
+            gossipService0.start();
+            URI url;
+            try {
+                url = new URI("udp://" + myIpAddress + ":" + 50001);
+            } catch (URISyntaxException var9) {
+                throw new RuntimeException(var9);
+            }
+            GossipService gossipService = new GossipService(cluster,
+                    url, "seed3", new HashMap(), startupMembers, settings,
+                    new GossipListener() {
+                        @Override
+                        public void gossipEvent(GossipMember member, GossipState state) {
+                            System.out.println(System.currentTimeMillis() + " "
+                                    + member + " " + state);
+                        }
+                    },
+                    new MetricRegistry());
+            clients.add(gossipService);
+            gossipService.start();
 
-	@Test
-	public void test3(){
-		try {
-			GossipSettings settings = new GossipSettings();
-			settings.setGossipInterval(1);
-			settings.setCleanupInterval(1000);
-			List<GossipService> clients = new ArrayList();
-			String myIpAddress = InetAddress.getLocalHost().getHostAddress();
-			String cluster = "My Gossip Cluster 1";
-			List<GossipMember> startupMembers = new ArrayList();
+            while (true) {
+                sleep(2000L);
+                if (!clients.isEmpty()) {
+                    if (!clients.isEmpty()) {
+                        List<LocalGossipMember> localGossipMemberList1 = clients.get(0).getGossipManager().getLiveMembers();
+                        for (LocalGossipMember localGossipMember : localGossipMemberList1) {
+                            System.out.print(" " + localGossipMember.getUri());
 
-			URI url;
-			try {
-				url = new URI("udp://" + myIpAddress + ":" + 50004);
-			} catch (URISyntaxException var9) {
-				throw new RuntimeException(var9);
-			}
-			GossipService gossipService = new GossipService(cluster,
-					url, "seed4", new HashMap(), startupMembers, settings,
-					new GossipListener() {
-						@Override
-						public void gossipEvent(GossipMember member, GossipState state) {
-							System.out.println(System.currentTimeMillis() + " "
-									+ member + " " + state);
-						}
-					},
-					new MetricRegistry());
-			clients.add(gossipService);
-			gossipService.start();
+                        }
+                    }
+                }
+            }
+            //((GossipService) clients.get(0)).getGossipManager().shutdown();
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
 
-			while(true) {
-				sleep(2000L);
-				if(!clients.isEmpty()) {
-					List<LocalGossipMember> localGossipMemberList1 = clients.get(0).getGossipManager().getLiveMembers();
-					for (LocalGossipMember localGossipMember : localGossipMemberList1) {
-						System.out.print(" " + localGossipMember.getUri());
-					}
-				}
-			}
+    }
 
-		} catch (UnknownHostException var10) {
-			var10.printStackTrace();
-		} catch (InterruptedException var11) {
-			var11.printStackTrace();
-		}
-	}
+    @Test
+    public void test3() {
+        try {
+            GossipSettings settings = new GossipSettings();
+            settings.setGossipInterval(1);
+            settings.setCleanupInterval(1000);
+            List<GossipService> clients = new ArrayList();
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster 1";
+            List<GossipMember> startupMembers = new ArrayList();
 
+            URI seedUrl;
+            try {
+                seedUrl = new URI("udp://" + myIpAddress + ":" + 11111);
+            } catch (URISyntaxException var9) {
+                throw new RuntimeException(var9);
+            }
+            startupMembers.add(new RemoteGossipMember(cluster, seedUrl, "seed0", 0L, new HashMap()));
+            /*GossipService gossipService0 = new GossipService(cluster, seedUrl, "", new HashMap(), startupMembers, settings, (GossipListener) null, new MetricRegistry());
+			gossipService0.start();*/
+            URI url;
+            try {
+                url = new URI("udp://" + myIpAddress + ":" + 50002);
+            } catch (URISyntaxException var9) {
+                throw new RuntimeException(var9);
+            }
+            GossipService gossipService = new GossipService(cluster,
+                    url, "seed3", new HashMap(), startupMembers, settings,
+                    new GossipListener() {
+                        @Override
+                        public void gossipEvent(GossipMember member, GossipState state) {
+                            System.out.println(System.currentTimeMillis() + " "
+                                    + member + " " + state);
+                        }
+                    },
+                    new MetricRegistry());
+            clients.add(gossipService);
+            gossipService.start();
+
+            while (true) {
+                sleep(2000L);
+                if (!clients.isEmpty()) {
+                    if (!clients.isEmpty()) {
+                        List<LocalGossipMember> localGossipMemberList1 = clients.get(0).getGossipManager().getLiveMembers();
+                        for (LocalGossipMember localGossipMember : localGossipMemberList1) {
+                            System.out.print(" " + localGossipMember.getUri());
+
+                        }
+                    }
+                }
+            }
+            //((GossipService) clients.get(0)).getGossipManager().shutdown();
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void test4() {
+        try {
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster 1";
+            GossipSettings s = new GossipSettings();
+            s.setWindowSize(10);
+            s.setConvictThreshold(1.0D);
+            s.setGossipInterval(10);
+            GossipService gossipService = new GossipService("mycluster", URI.create("udp://localhost:10000"), "0", new HashMap(), Arrays.asList(new RemoteGossipMember("mycluster", URI.create("udp://localhost:10000"), "0")), s, (a, b) -> {
+            }, new MetricRegistry());
+            gossipService.start();
+
+            while (true) {
+                System.out.println("Live: " + gossipService.getGossipManager().getLiveMembers());
+                System.out.println("Dead: " + gossipService.getGossipManager().getDeadMembers());
+                Thread.sleep(2000L);
+            }
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test5() {
+        try {
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster 1";
+            GossipSettings s = new GossipSettings();
+            s.setWindowSize(10);
+            s.setConvictThreshold(1.0D);
+            s.setGossipInterval(10);
+            GossipService gossipService = new GossipService("mycluster", URI.create("udp://localhost:10001"), "1", new HashMap(), Arrays.asList(new RemoteGossipMember("mycluster", URI.create("udp://localhost:10000"), "0")), s, (a, b) -> {
+            }, new MetricRegistry());
+            gossipService.start();
+
+            while (true) {
+                System.out.println("Live: " + gossipService.getGossipManager().getLiveMembers());
+                System.out.println("Dead: " + gossipService.getGossipManager().getDeadMembers());
+                Thread.sleep(2000L);
+            }
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test6() {
+        try {
+            String myIpAddress = InetAddress.getLocalHost().getHostAddress();
+            String cluster = "My Gossip Cluster 1";
+            GossipSettings s = new GossipSettings();
+            s.setWindowSize(10);
+            s.setConvictThreshold(1.0D);
+            s.setGossipInterval(10);
+            GossipService gossipService = new GossipService("mycluster", URI.create("udp://localhost:10002"), "2", new HashMap(), Arrays.asList(new RemoteGossipMember("mycluster", URI.create("udp://localhost:10000"), "0")), s, (a, b) -> {
+            }, new MetricRegistry());
+            gossipService.start();
+
+            while (true) {
+                System.out.println("Live: " + gossipService.getGossipManager().getLiveMembers());
+                System.out.println("Dead: " + gossipService.getGossipManager().getDeadMembers());
+                Thread.sleep(2000L);
+            }
+        } catch (UnknownHostException var10) {
+            var10.printStackTrace();
+        } catch (InterruptedException var11) {
+            var11.printStackTrace();
+        }
+    }
 }
